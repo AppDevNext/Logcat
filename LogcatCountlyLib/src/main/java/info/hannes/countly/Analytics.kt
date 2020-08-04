d68a4dfe524a23c2f790250fe9e9c2a68299341a
Hannes Achleitner <hannes.software@gmx.at>
2020-08-04 06:46:53 +0200

    Countly

---
+++ LogcatCountlyLib/src/main/java/info/hannes/countly/Analytics.kt
@@ -0,0 +1,89 @@
+package info.hannes.countly
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.provider.Settings
+import info.hannes.logcat.BuildConfig
+import ly.count.android.sdk.Countly
+import ly.count.android.sdk.CountlyConfig
+import ly.count.android.sdk.DeviceId
+
+
+@Suppress("PrivatePropertyName")
+class Analytics : IAnalytics {
+
+    var countlyInstance: Countly = Countly.sharedInstance()
+        private set
+
+    override fun isInitialized(): Boolean {
+        return countlyInstance.isInitialized
+    }
+
+    override fun recordEvent(event: String) {
+        if (isInitialized()) {
+            countlyInstance.recordEvent(event, segmentation, 1)
+        }
+    }
+
+    override fun recordError(message: String) {
+        if (isInitialized()) {
+            countlyInstance.recordHandledException(RuntimeException(message))
+        }
+    }
+
+    override fun recordError(throwable: Throwable) {
+        if (isInitialized()) {
+            countlyInstance.recordHandledException(throwable)
+        }
+    }
+
+    override fun recordWarning(message: String) {
+        if (isInitialized()) {
+            countlyInstance.recordHandledException(RuntimeException(message))
+        }
+    }
+
+    override fun onStart(activity: Activity?) {
+        if (isInitialized()) {
+            countlyInstance.onStart(activity)
+        }
+    }
+
+    override fun onStop() {
+        if (isInitialized()) {
+            countlyInstance.onStop()
+        }
+    }
+
+    companion object {
+        private const val COUNTLY_HOST = "https://countly.apinext.bmw.com"
+
+        val segmentation = HashMap<String, String>()
+
+        @SuppressLint("HardwareIds")
+        fun initAnalytics(context: Context, loggingEnabled: Boolean = false, countlyKey: String) {
+            var version = ""
+            try {
+                val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+                version = pInfo.versionName
+            } catch (ignore: PackageManager.NameNotFoundException) {
+            }
+            segmentation["app_version"] = version
+            segmentation["logging_version"] = BuildConfig.VERSION_NAME
+
+            val config = CountlyConfig()
+                    .setAppKey(countlyKey)
+                    .setContext(context)
+                    .setDeviceId(Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID))
+                    .setIdMode(DeviceId.Type.DEVELOPER_SUPPLIED)
+                    .setServerURL(COUNTLY_HOST)
+                    .setLoggingEnabled(loggingEnabled)
+                    .setViewTracking(true)
+                    .setHttpPostForced(true)
+                    .enableCrashReporting()
+            Countly.sharedInstance().init(config)
+        }
+    }
+}
---
+++ LogcatCountlyLib/src/main/java/info/hannes/countly/CountlyTree.kt
@@ -0,0 +1,39 @@
+package info.hannes.countly
+
+import android.util.Log
+import info.hannes.timber.DebugTree
+
+class CountlyTree(private val analytics: Analytics, private val serverIgnoreToken: String? = null) : DebugTree() {
+
+    // some regex explanation :
+    // $t sign to search
+    // . any char
+    // + minimum once
+    // ? null or once
+    // | or
+    // # beginning with #
+    // [^#] any char except #
+    // *$ till end of line
+
+    // short variable to avoid lint warning "Duplicate char n inside Character class" in following line
+    private val t = serverIgnoreToken
+    private val regex: Regex = "$t.+?$t|$t[^$t]*$".toRegex()
+
+    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+        // we ignore INFO, DEBUG and VERBOSE
+        if (priority <= Log.INFO) {
+            return
+        }
+        var localMessage = message
+        serverIgnoreToken?.let {
+            localMessage = regex.replace(message, "")
+        }
+
+        when {
+            throwable != null -> analytics.recordError(throwable)
+            priority == Log.WARN -> analytics.recordEvent(localMessage)
+            else -> analytics.recordError(localMessage)
+        }
+    }
+
+}
