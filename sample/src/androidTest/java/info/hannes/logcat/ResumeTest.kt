073caa3b4ba54cfd3b844bd63cadcd6f3e05b841
Hannes Achleitner <hannes.software@gmx.at>
2021-05-02 08:22:05 +0200

    Resume test
    Search for duplicate items

---
+++ sample/src/androidTest/java/info/hannes/logcat/ResumeTest.kt
@@ -0,0 +1,47 @@
+package info.hannes.logcat
+
+import android.Manifest
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.uiautomator.UiDevice
+import info.hannes.logcat.utils.RecyclerViewItemDuplicateAssertion
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class ResumeTest {
+
+    @get:Rule
+    var mActivityTestRule = ActivityScenarioRule(LogfileActivity::class.java)
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    @Test
+    @Ignore("Currently we have duplicate lines after resume from tasklist")
+    fun checkForDuplicateAfterRressRecentApps() {
+        // Might be a good idea to initialize it somewhere else
+        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        uiDevice.pressRecentApps()
+        Thread.sleep(WAIT)
+        uiDevice.pressRecentApps()
+        Thread.sleep(WAIT)
+
+        val recycler = Espresso.onView(ViewMatchers.withId(R.id.log_recycler))
+        recycler.check(RecyclerViewItemDuplicateAssertion())
+    }
+
+    companion object {
+        const val WAIT = 300L
+    }
+}
