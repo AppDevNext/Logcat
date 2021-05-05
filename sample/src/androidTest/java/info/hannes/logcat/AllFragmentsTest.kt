9ab98d2b120c5927eab8256d333e4bd4b27fe5d4
Hannes Achleitner <hannes.software@gmx.at>
2021-05-05 11:49:27 +0200

    All fragment test with items

--- sample/src/androidTest/java/info/hannes/logcat/AllFragmentsTest.kt
+++ sample/src/androidTest/java/info/hannes/logcat/AllFragmentsTest.kt
@@ -8,7 +8,10 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.rule.GrantPermissionRule
+import com.moka.lib.assertions.WaitingAssertion
 import com.moka.utils.Screenshot
+import info.hannes.logcat.utils.MatchOperator
+import info.hannes.logcat.utils.RecyclerViewItemCountAssertion
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
@@ -23,17 +26,26 @@ class AllFragmentsTest {
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     @Test
     fun basicTest() {
-        onView(allOf(withContentDescription("All log"),
+        onView(
+            allOf(
+                withContentDescription("All log"),
                 withParent(withId(R.id.action_bar)),
-                isDisplayed()))
+                isDisplayed()
+            )
+        )
 
         onView(withText("Logcat")).check(ViewAssertions.matches(isDisplayed()))
         onView(withText("Logfile")).check(ViewAssertions.matches(isDisplayed()))
+
+        WaitingAssertion.checkAssertion(R.id.log_recycler, isDisplayed(), 1500)
+        onView(allOf(withId(R.id.log_recycler), isDisplayed()))
+            .check(RecyclerViewItemCountAssertion(2, MatchOperator.GRATER_EQUAL))
 
         Screenshot.takeScreenshot("End")
     }
