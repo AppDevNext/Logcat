2bae0599f5969f831d657e339a1e97fcd831405e
Hannes Achleitner <hannes.software@gmx.at>
2022-07-19 16:17:13 +0200

    Sample packagename

--- sample/src/main/java/info/hannes/logcat/sample/CrashlyticApplication.kt
+++ sample/src/main/java/info/hannes/logcat/sample/CrashlyticApplication.kt
@@ -1,4 +1,4 @@
-package info.hannes.logcat
+package info.hannes.logcat.sample
 
 import android.annotation.SuppressLint
 import android.os.Handler
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.provider.Settings
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import info.hannes.crashlytic.CrashlyticsTree
+import info.hannes.logcat.LoggingApplication
 import info.hannes.timber.FileLoggingTree
 import timber.log.Timber
 
@@ -20,7 +21,7 @@ class CrashlyticApplication : LoggingApplication() {
             Timber.plant(FileLoggingTree(it, this))
         }
 
-        FirebaseCrashlytics.getInstance().setCustomKey("VERSION_NAME", BuildConfig.VERSIONNAME)
+        FirebaseCrashlytics.getInstance().setCustomKey("VERSION_NAME", info.hannes.logcat.ui.BuildConfig.VERSIONNAME)
         Timber.plant(CrashlyticsTree(Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)))
 
         Timber.d("Debug test")
