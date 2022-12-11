15c5f3a6c93e269c1356129c2d08abb949a0a0e6
Hannes Achleitner <hannes.software@gmx.at>
2022-12-11 07:53:06 +0100

    View binding

--- sample/src/main/java/info/hannes/logcat/sample/MainActivity.kt
+++ sample/src/main/java/info/hannes/logcat/sample/MainActivity.kt
@@ -6,50 +6,41 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import info.hannes.logcat.sample.databinding.ActivityMainBinding
 import info.hannes.logcat.ui.BothLogActivity
 import info.hannes.logcat.ui.LogcatActivity
 import info.hannes.logcat.ui.LogfileActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.containMain.toolbar)
 
         // enable ActionBar app icon to behave as action to toggle nav drawer
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        if (navigationView != null) {
-            setupDrawerContent(navigationView)
-        }
-
+        setupDrawerContent(binding.navView)
     }
 
     private fun setupDrawerContent(navigationView: NavigationView) {
         navigationView.setNavigationItemSelectedListener { menuItem ->
             menuItem.isChecked = true
             selectNavigationItem(menuItem.itemId)
-            drawerLayout.closeDrawers()
+            binding.drawerLayout.closeDrawers()
             true
         }
-
     }
 
     private fun selectNavigationItem(itemId: Int) {
-
         when (itemId) {
             R.id.nav_drawer_logcat -> startActivity(Intent(this, LogcatActivity::class.java))
             R.id.nav_drawer_logfile -> startActivity(Intent(this, LogfileActivity::class.java))
@@ -71,7 +62,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                drawerLayout.openDrawer(GravityCompat.START)
+                binding.drawerLayout.openDrawer(GravityCompat.START)
             }
             R.id.nav_drawer_logcat -> {
                 startActivity(Intent(this, LogcatActivity::class.java))
