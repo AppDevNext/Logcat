35715a6ecedb30b4ec64a0806b8a22004f6999cc
Hannes Achleitner <hannes.software@gmx.at>
2022-12-11 08:33:17 +0100

    More viewbinding

--- LogcatCoreUI/src/main/java/info/hannes/logcat/ui/BothLogActivity.kt
+++ LogcatCoreUI/src/main/java/info/hannes/logcat/ui/BothLogActivity.kt
@@ -2,40 +2,41 @@ package info.hannes.logcat.ui
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import info.hannes.logcat.ui.databinding.ActivityLogBinding
 
 class BothLogActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLogBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log)
+        binding = ActivityLogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // Check that the activity is using the layout version with the fragment_container FrameLayout
-        if (findViewById<View>(R.id.fragment_container) != null) {
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return
-            }
-
-            // Create a new Fragment to be placed in the activity layout
-            val bothLogFragment = BothLogsFragment.newInstance(
-                "logfile.log",
-                "search logfile",
-                "search logcat",
-                "your@mail.com"
-            )
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            // firstFragment.arguments = intent.extras
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container, bothLogFragment).commit()
+        // However, if we're being restored from a previous state,
+        // then we don't need to do anything and should return or else
+        // we could end up with overlapping fragments.
+        if (savedInstanceState != null) {
+            return
         }
+
+        // Create a new Fragment to be placed in the activity layout
+        val bothLogFragment = BothLogsFragment.newInstance(
+            "logfile.log",
+            "search logfile",
+            "search logcat",
+            "your@mail.com"
+        )
+
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        // firstFragment.arguments = intent.extras
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, bothLogFragment).commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
