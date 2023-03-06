cf8b6e8323d4b69f5a88057de104336a1d0e1d96
Hannes Achleitner <hannes.software@gmx.at>
2023-03-06 06:25:02 +0100

    ktLint findings

--- LogcatCoreLib/src/main/java/info/hannes/timber/DebugFormatTree.kt
+++ LogcatCoreLib/src/main/java/info/hannes/timber/DebugFormatTree.kt
@@ -8,12 +8,12 @@ open class DebugFormatTree : Timber.DebugTree() {
 
     override fun createStackElementTag(element: StackTraceElement): String? {
         return String.format(
-                "(%s:%d) %s.%s()",
-                element.fileName,
-                element.lineNumber, // format ensures line numbers have at least 3 places to align consecutive output from the same file
-                // method is fully qualified only when class differs on filename otherwise it can be cropped on long lambda expressions
-                super.createStackElementTag(element)?.replaceFirst(element.fileName.takeWhile { it != '.' }, ""),
-                element.methodName
+            "(%s:%d) %s.%s()",
+            element.fileName,
+            element.lineNumber, // format ensures line numbers have at least 3 places to align consecutive output from the same file
+            // method is fully qualified only when class differs on filename otherwise it can be cropped on long lambda expressions
+            super.createStackElementTag(element)?.replaceFirst(element.fileName.takeWhile { it != '.' }, ""),
+            element.methodName
         )
     }
 
