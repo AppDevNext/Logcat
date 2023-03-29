f23c8834f3eaaedc73f3176d6c4ce6e7ee643358
Hannes Achleitner <hannes.software@gmx.at>
2023-03-29 06:23:33 +0200

    Fix compile error

--- LogcatCoreLib/src/main/java/info/hannes/logcat/Event.kt
+++ LogcatCoreLib/src/main/java/info/hannes/logcat/Event.kt
@@ -55,8 +55,8 @@ open class Event<out T>(private val content: T) {
 @Suppress("unused")
 class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
 
-    override fun onChanged(event: Event<T>?) {
-        event?.getContentIfNotHandled()?.let {
+    override fun onChanged(event: Event<T>) {
+        event.getContentIfNotHandled()?.let {
             onEventUnhandledContent(it)
         }
     }
