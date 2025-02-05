d9fb2bf8f1dc54e9f080811efc55c6531e7b6a00
Hannes Achleitner <hannes.software@gmx.at>
2025-02-05 06:27:37 +0100

    Bump test libs

--- app/src/androidTest/java/info/hannes/logcat/AllFragmentsTest.kt
+++ app/src/androidTest/java/info/hannes/logcat/AllFragmentsTest.kt
@@ -1,14 +1,15 @@
 package info.hannes.logcat
 
+import android.graphics.Bitmap
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.captureToBitmap
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import info.hannes.logcat.ui.BothLogActivity
-import org.hamcrest.Matchers.allOf
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -33,8 +34,7 @@ class AllFragmentsTest {
         onView(withText("Logfile")).check(ViewAssertions.matches(isDisplayed()))
 
         onView(isRoot())
-            .captureToBitmap()
-            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
+            .perform(captureToBitmap { bitmap: Bitmap -> bitmap.writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}") })
     }
 
 }
