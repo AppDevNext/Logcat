85a216068b67833236aac6be3453b59f3898b500
Hannes Achleitner <hannes.software@gmx.at>
2026-07-13 07:02:07 +0200

    Bump irebase-crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:14.2.0")
         classpath("com.google.gms:google-services:4.4.4")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.7")
     }
 }
 
